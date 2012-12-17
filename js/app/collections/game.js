e90e51fdda457b6d9f115994b366e295d16869b8
Jeroen Hoebe <hoebejeroen@gmail.com>
2012-12-17 19:30:48 +0100

    change dataset to numbers and add compare

--- js/app/collections/game.js
+++ js/app/collections/game.js
@@ -1,5 +1,9 @@
 // define collection
 FED.Game = Backbone.Collection.extend({
-  model: FED.Set
+  model: FED.Set,
+
+  comparator: function (a,b) {
+    return b.get("set") - a.get("set");
+  }
 });
 // console.log("game collection has loaded!");
