f0c02f5064536f27f996e28c702f8d62ddb4d502
Dennis Tel <dennistel90@gmail.com>
2013-01-21 13:32:28 +0100

    fix

--- js/app/collections/matchesCollection.js
+++ js/app/collections/matchesCollection.js
@@ -7,18 +7,6 @@ FED.Matches = Backbone.Collection.extend({
         return data.objects;
     },
 
-    // hetzelfde als for each
-    // comparator: function(a, b) {
-    //     var a = a.get("team1");
-    //     var b = b.get("team1");
-    //     if(a < b) {
-    //       return 1;
-    //     } else if (a > b) {
-    //       return -1;
-    //     } else {
-    //       return 0;
-    //     }
-    // }
     comparator : function(matches) {
       return matches.get("date");
     }
