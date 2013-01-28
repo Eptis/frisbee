65ac95049ff62d41a632b396b72d1f5d6f96bfa3
Jeroen Hoebe <hoebejeroen@gmail.com>
2013-01-28 22:07:49 +0100

    mijn access token

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 
 FED.config = {
     tournamentID: 18519,
-    access_token: 'e08a55d872',
-    header_access:"bearer e08a55d872",
+    access_token: 'ec138cbcfd',
+    header_access:"bearer ec138cbcfd",
     api_url: 'https://api.leaguevine.com/v1/tournaments/',
     schedule_api_url: 'https://api.leaguevine.com/v1/games/?pool_id=18744',
     pool_api_url: 'https://api.leaguevine.com/v1/pools/?pool_ids=%5B18744%5D&tournament_id=18519',
@@ -12,3 +12,5 @@ FED.config = {
     season_id: 20167
 }
 
+// access_token jeroen: ec138cbcfd
+// access_token joost: e08a55d872
